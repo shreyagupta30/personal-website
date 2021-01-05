afdeb566966bfcda6544c1263080332cec85e21d
shreyagupta30 <shreyagupta3011@gmail.com>
2021-01-05 18:24:34 +0530

    Updates

---
+++ precache-manifest.62d31810e87862d2d602bc06d9ff3f10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84897f86d05333e1c99ef6f144de0b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd11f2798253a1928fb",
+    "url": "/static/js/2.0889f929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0889f929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643939f38355914c9be4",
+    "url": "/static/js/main.1193e0d4.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
