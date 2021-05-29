5636948f0f68583b1a86cb31d9f469c6fe0fd984
shreyagupta30 <shreyagupta3011@gmail.com>
2021-05-29 18:18:09 +0530

    add resume

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -238,7 +238,7 @@ const Navbar = () => {
             <a
               aria-label="Open Shreya's Resume in a new tab"
               target="blank"
-              href="https://drive.google.com/file/d/1aEIhEWWQzJnr7cZq-U5oJqWHKC1PrPfY/view?usp=sharing"
+              href="https://drive.google.com/file/d/18ylWdP4YBvz87sn3Hss-oXqPxkB5_Pzj/view?usp=sharing"
               className="waves-effect waves-dark grey-text text-darken-3"
             >
               <i className="mdi-action-description small"></i>
